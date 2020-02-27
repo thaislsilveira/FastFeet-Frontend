eec04fcbf8134f8cfca39dbabb110bd2bb6f28e0
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-02-27 11:10:36 -0300

    Persistindo autenticação

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persistReducers';
 
 import rootReduter from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -13,8 +16,8 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReduter, middlewares);
-
+const store = createStore(persistReducers(rootReduter), middlewares);
+const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
