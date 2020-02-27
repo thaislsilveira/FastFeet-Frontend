0911c3b69a88230207bfddbdf7ecc46dddf13901
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-02-27 11:43:08 -0300

    Exibindo toasts

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -17,6 +17,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
   }
@@ -31,7 +32,9 @@ export default function SignIn() {
           <h3>SUA SENHA</h3>
           <Input name="password" type="password" placeholder="*************" />
 
-          <button type="submit">Entrar no sistema</button>
+          <button type="submit">
+            {loading ? 'Carregando...' : 'Entrar no sistema'}
+          </button>
 
           <Link to="/register">Criar conta gratuita</Link>
         </Form>
