37fc68437405654ee72f4f88c9fc01275fe5b57f
thailsilveira <thais_lsilveira@hotmail.com>
2020-03-02 21:29:46 -0300

    update - entregadores

---
+++ src/pages/EditDeliveryman/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import * as Yup from 'yup';
+import { Input, Form } from '@rocketseat/unform';
+import { MdKeyboardArrowLeft, MdCheck } from 'react-icons/md';
+import { toast } from 'react-toastify';
+import { Link, useParams } from 'react-router-dom';
+import api from '../../services/api';
+
+import ActionHeader from '../../components/ActionHeader';
+import ActionContent from '../../components/ActionContent';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+});
+
+export default function EditDeliveryman() {
+  const [deliverymanData, setDeliverymanData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function getDeliverymanData() {
+      const response = await api.get(`deliverymen/${id}`);
+      const initialData = {
+        name: response.data[0].name,
+        email: response.data[0].email,
+      };
+      console.log(response);
+      setDeliverymanData(initialData);
+    }
+    getDeliverymanData();
+  }, [id]);
+
+  async function handleSubmit({ name, email }) {
+    try {
+      await api.put(`deliverymen/${id}`, {
+        name,
+        email,
+      });
+
+      toast.success('Entregador alterado com sucesso');
+    } catch (err) {
+      toast.error(err.response.data.error);
+    }
+  }
+
+  return (
+    <>
+      <ActionHeader>
+        <div>
+          <span>Edição de Entregadores</span>
+          <aside>
+            <Link className="prevPage" to="/deliverymen">
+              <MdKeyboardArrowLeft size={20} color="#fff" />
+              VOLTAR
+            </Link>
+            <button type="submit" form="deliverymen-form">
+              <MdCheck size={20} color="#fff" />
+              SALVAR
+            </button>
+          </aside>
+        </div>
+      </ActionHeader>
+      <ActionContent>
+        <Form
+          onSubmit={handleSubmit}
+          initialData={deliverymanData}
+          schema={schema}
+          id="deliverymen-form"
+        >
+          <label htmlFor="name">Nome</label>
+          <Input name="name" type="text" placeholder="John Doe" />
+          <label htmlFor="name">Email</label>
+          <Input
+            name="email"
+            type="email"
+            placeholder="exemplo@rocketseat.com"
+          />
+        </Form>
+      </ActionContent>
+    </>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import Order from '../pages/Order';
 import AddOrder from '../pages/AddOrder';
 import Deliveryman from '../pages/Deliveryman';
 import AddDeliverymen from '../pages/AddDeliveryman';
+import EditDeliverymen from '../pages/EditDeliveryman';
 import Recipient from '../pages/Recipient';
 import AddRecipient from '../pages/AddRecipient';
 import Problem from '../pages/Problem';
@@ -15,17 +16,18 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/orders" component={Order} isPrivate />
+      <Route exact path="/orders" component={Order} isPrivate />
       <Route path="/register/orders" component={AddOrder} isPrivate />
-      <Route path="/deliverymen" component={Deliveryman} isPrivate />
+      <Route exact path="/deliverymen" component={Deliveryman} isPrivate />
       <Route
         path="/register/deliverymen"
         component={AddDeliverymen}
         isPrivate
       />
-      <Route path="/recipients" component={Recipient} isPrivate />
+      <Route path="/deliverymen/:id" component={EditDeliverymen} isPrivate />
+      <Route exact path="/recipients" component={Recipient} isPrivate />
       <Route path="/register/recipients" component={AddRecipient} isPrivate />
-      <Route path="/deliveryproblems" component={Problem} isPrivate />
+      <Route exact path="/deliveryproblems" component={Problem} isPrivate />
     </Switch>
   );
 }
